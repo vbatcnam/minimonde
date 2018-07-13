3ab63eb1dfbc2bedb03e988420e10477299a1dd4
vbatcnam <40175721+vbatcnam@users.noreply.github.com>
2018-07-13 17:35:09 +0200

    add machine.js

---
+++ javascript/machine.js
@@ -0,0 +1,7 @@
+//le moteur qui exécute les programmes
+//-------------------------------------
+var moteur = SC.machine(30);// toutes les 30 millisecondes il y a une macro étape (ou instant)
+
+//On ajoute les cubes au moteur
+//-------------------------------------
+//moteur.addProgram(cube);
