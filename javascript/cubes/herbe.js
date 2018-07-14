9afada4c53855b677a936e83f324ead46dd7a84e
vbatcnam <vero@famille.fr>
2018-07-14 17:42:53 +0200

    modifié herbe.js ajouté un id fonction getEltSVG()soucis car eltSVG est null du coup getEchelle(elt) ne peut pas marcher

--- javascript/cubes/herbe.js
+++ javascript/cubes/herbe.js
@@ -1,20 +1,40 @@
 class Herbe extends Vegetal{
-	constructor(){
-		super();
+	constructor(num){
+		super(num);
+		this.id = "herbe_" + this.num;
 	}
 	
-	//Il faut les positionner de manière aléatoire 
+	
+	//Il faudra les positionner de manière aléatoire 
 	draw(x,y){
 		var prairie = document.getElementById("prairie");
 		var zoneHerbe = document.createElement("object");
+		zoneHerbe.id = this.id;
 		zoneHerbe.className = "herbe";
 		zoneHerbe.type = "image/svg+xml";
-		zoneHerbe.data = "image/herbeSFW.svg";
+		zoneHerbe.data = "image/herbe.svg";
 		prairie.appendChild(zoneHerbe);
 	}
+	
+	getEltSVG(){
+		let eltObject = document.getElementById(this.id);
+		console.log(eltObject);
+		let eltSVG = eltObject.contentDocument;
+		console.log(eltSVG);
+		return eltSVG;
+	}
+	
+	getEchelle(elt){
+		return array_getEchelle(elt);
+	}
+	
 }
 
 //test
 for(var i = 0; i<5; i++){
-	new Herbe().draw(i+2, i+4);
+	let herbe = new Herbe(i+1);
+	herbe.draw(i+2, i+4);
+	var eltSvg = herbe.getEltSVG();
+	var attributs = eltSvg;
+	herbe.getEchelle(eltSvg);
 }
