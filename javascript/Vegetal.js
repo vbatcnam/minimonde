857edc532d6be0be48a6dda65f328662ad02fc4e
vbatcnam <vero@famille.fr>
2018-07-16 10:44:31 +0200

    retiré alive = true ; inutile je pense this.killMe = SC.evt("kill");seuls certains type de vegetaux meurent

--- javascript/Vegetal.js
+++ javascript/Vegetal.js
@@ -2,8 +2,6 @@ class Vegetal{
 	constructor(num){
 		this.num = num;
 		this.taille = 0; //Naissance (De 0 à 5 étapes)
-		this.alive = true;
-		this.killMe = SC.evt("kill");
 	}
 	grandi(){
 		this.taille += 1;
