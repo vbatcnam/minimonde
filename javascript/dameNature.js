32618e82a751a8a613ddeca095cc7779b264c9aa
vbatcnam <vero@famille.fr>
2018-08-23 21:15:59 +0200

    ajout dameNature.js

---
+++ javascript/dameNature.js
@@ -0,0 +1,13 @@
+/**
+	Elle s’occupe de créer les plantes.
+	Elle regarde si il y a de la place dans la prairie et fait naître une herbe ou une fleur
+*/
+class Nature{
+	constructor(){}
+	
+	FaitNaitre(objet){}
+	
+	verifieSiPlaceLibre(){}
+}
+
+var dameNature = new Nature();
