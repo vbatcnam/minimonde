6a244e0d69a1a2cd9e6bbb46922f4e777cc30a14
vbatcnam <vero@famille.fr>
2018-11-13 19:01:47 +0100

    nettoyage

--- javascript/metteurEnScene.js
+++ javascript/metteurEnScene.js
@@ -1,22 +1,12 @@
 /**
-	C'est un objet centrale qui gère tous les cubes.
+	C'est le fichier centrale qui ajoute tous tous les cubes au monde.
 */
 
-//le moteur qui exécute les programmes
-//-------------------------------------
-//var moteur = SC.machine(30);// toutes les 30 millisecondes il y a une macro étape (ou instant)
-
-//On ajoute les cubes au moteur
-//-------------------------------------
-//moteur.addProgram(cube);
-
-//ajout de la prairie
-//moteur.addProgram(cubePrairie);
 monde.addActor(cubePrairie);
 
 //l'herbe
 // for(let tab_CubeHerbe of cubePrairie){
   // for(let cube of tab_CubeHerbe){
-    // moteur.addProgram(cube);
+    // monde.addActor(cube);
     // }
 // }
