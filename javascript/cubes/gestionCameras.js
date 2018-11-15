9d96c4dc98a6cb976595db7ee804cc8908f18219
vbatcnam <vero@famille.fr>
2018-11-15 17:42:48 +0100

    restructuration, ajout des layer à la construction

--- javascript/cubes/gestionCameras.js
+++ javascript/cubes/gestionCameras.js
@@ -3,11 +3,26 @@
 class Camera2D extends SCCube{
 	constructor(){
 		super();
+		this.createSvgElement();
+		document.getElementById('champCamera').appendChild(this.svgElement);
+		this.tab_layers = [];
+		this.remplirTabDeLayers();
+	}
+	
+	createSvgElement(){
 		this.svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		this.svgElement.id = 'svgPrincipal';
 		this.svgElement.setAttribute("width", innerWidth);
 		this.svgElement.setAttribute("height", innerHeight);
-		document.getElementById('champCamera').appendChild(this.svgElement);
+
+	}
+	
+	remplirTabDeLayers(){
+		for (var i = 0; i<5; i++){
+			this.tab_layers[i] = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+			this.tab_layers[i].id = 'layer_'+i;
+			this.svgElement.appendChild(this.tab_layers[i]);
+		}
 	}
 	
 	//Le fond
