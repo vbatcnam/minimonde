3debe6181c55cdc28fd63c36acbc411de9e2e54a
vbatcnam <vero@famille.fr>
2018-12-14 20:15:23 +0100

    ajout commentaires explicatif

--- javascript/cubes/Horloge.js
+++ javascript/cubes/Horloge.js
@@ -1,10 +1,14 @@
 'use strict';
 
 //On démarre le monde
-var monde = SC.machine(30);//Temps de l'animation et des calculs et pas le temps du monde.
+var monde = SC.machine(25);//Temps de l'animation et des calculs et pas le temps du monde. 
+/**25 est assez bien pour une fluidité des animations(40 tics/s). De plus c'est chiffre rond 1000 ms/25Ms tombe juste.*/
 
-
-//Temps du monde : 1 heure du miniMonde correspond à 1 mn temps IRL
+/***
+	Temps du monde : 1 heure du miniMonde correspond à 1 mn temps IRL
+	Le soleil fait un tour complet tous les 57600 tics 
+	1 jour = 1440 secondes 
+*/
 class Horloge extends SCCube{
 	constructor(){
 		super();
