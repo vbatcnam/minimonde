e750c31a313e899cd0dab43bc2f41d08dfef37bd
vbatcnam <vero@famille.fr>
2019-03-08 23:27:08 +0100

    réorganisation en cours (ça ne marche plus)

--- javascript/cubes/vache.js
+++ javascript/cubes/vache.js
@@ -22,13 +22,12 @@ class Vache extends SCCube{
 		this.age = 0; 
 		this.faim = 0; //n'a pas faim (100 a très faim)
 		this.eating = false;
-		this.nourritureEnCours = {};
+		this.nourritureVisee = {};
 		this.fatigue = 0; //n'est pas fatigué (100 est épuisé)
 		this.dort = false;
 		this.lait = 0;
 		this.bouse = 0;
 		this.pas = 0.5; // 0.5 pixel/25 milisecondes 
-		this.nbreDePas = 0;
 		this.destination = {};
 	}
 
@@ -47,93 +46,62 @@ class Vache extends SCCube{
 	
 	//Écoute les herbes autour d'elle et trouve la plus proche
 	$on_monApparence(pArray_apparences){
-		//tete en position d'écoute
-//		console.log("J'écoute les herbes : " , this.teteActuelle);
-		if(!this.eating && this.teteActuelle != "teteProfil"){
-			consol("++++++++++++++++++++++++ ");
-			console.log("Je dois changer de tête ! ");
-			this.changement = {oldClass:this.teteActuelle , nouveau:teteProfil};
-			this.teteActuelle ='teteProfil';
-		}
+		//console.log("J'écoute les herbes : " , this.teteActuelle);
+		
 		let distanceMinimaleActuelle = Infinity;
 		for(let apparence of pArray_apparences){
-			if(apparence.espece == "herbe" && apparence.mangeable){
+			if(apparence.espece == "herbe" && this.nourritureVisee.taille > 0.01){
 				let distanceCourante = Calcule.getSqDistance2D(this.xTerrestre, this.zTerrestre, apparence.x, apparence.z);
 				if(distanceCourante < distanceMinimaleActuelle){
 					distanceMinimaleActuelle = distanceCourante;
 					this.destination.x = apparence.x; this.destination.z = apparence.z;
-					this.nourritureEnCours = apparence;
+					this.nourritureVisee = apparence;
 				}
 			}
 		}
-		this.setNbreDePas();
-		console.log("J'ai trouvé " + this.nourritureEnCours.id + " à manger : " , this.teteActuelle);
+		//console.log("J'ai trouvé " + this.nourritureVisee.id + " à manger : " , this.teteActuelle);
 	}
 	
-	setNbreDePas(){
-		let distance = Calcule.getSqDistance2D(this.xTerrestre, this.zTerrestre, this.destination.x, this.destination.z);
-		this.nbreDePas = Math.round(distance/this.pas);
+	//augmente la fatigue et la faim, diminue le poids
+	$actionForever_deplacement(){
+		//calculer le nombre de pas jusqu'à la cible
+		const distance = Calcule.getSqDistance2D(this.xTerrestre, this.zTerrestre, this.destination.x, this.destination.z);
+		const nbreDePas = Math.round(distance/this.pas);
 
-	}
-	
-	//anime l'animal, augmente la fatigue et la faim, diminue le poids
-	$actionForever_vie(){
-		//vérifie si la tête actuelle est celle par défaut (teteProfil)
-		console.log("Tête actuelle" , this.teteActuelle);
-		if(this.teteActuelle == "teteProfil"){
-			this.changement = undefined;
-		}else{
-			// this.changement = {oldClass:this.teteActuelle, nouveau:teteProfil};
-			// this.teteActuelle = "teteProfil";
-		}
 		//avance d'un pas en direction de l'herbe à manger
-		if(this.nbreDePas > 0){ 
-			this.xTerrestre += (this.destination.x - this.xTerrestre)/this.nbreDePas;
-			this.zTerrestre += (this.destination.z- this.zTerrestre)/this.nbreDePas;
-			console.log("J'avance d'un pas vers l'herbe à manger : " , this.teteActuelle);
+		if(nbreDePas > 0){ 
+			this.xTerrestre += (this.destination.x - this.xTerrestre)/nbreDePas;
+			this.zTerrestre += (this.destination.z- this.zTerrestre)/nbreDePas;
+			//console.log("J'avance d'un pas vers l'herbe à manger : " , this.teteActuelle);
 
 		}else{
-			console.log("===================================")
-			console.log("Je suis sur l'herbe à manger : " , this.teteActuelle);
-			this.broute();
+			// console.log("===================================")
+			// console.log("Je suis sur l'herbe à manger : " , this.teteActuelle);
 		}
+		this.aTable = nbreDePas <= 0;
 	}
 
 	//anime la mâchoire, diminue la faim et la fatigue, augmente le poids
-	broute(){
+	$actionForever_nutrition(){
+	//On ne se nourrit que si on est à table
+		if(!this.aTable){return};
+		
 		//y-a-t-il encore quelque chose à manger ?
-		console.log("L'herbe  ",this.nourritureEnCours.id);
-		console.log("L'herbe est mangeable ? ",this.nourritureEnCours.mangeable);
-		console.log("ais-je commencé à manger ? " , this.eating);
-		if(this.nourritureEnCours.mangeable){
+		// console.log("L'herbe  ",this.nourritureVisee.id);
+		// console.log("L'herbe est mangeable ? ",this.nourritureVisee.mangeable);
+		// console.log("ais-je commencé à manger ? " , this.eating);
+		if(){
 			this.eating = true;
 			
-			//Dois-je changer de tête ?
-			if(this.teteActuelle == "teteProfil"){
-				console.log("Je commence à manger")
-				this.changement = {oldClass:'teteProfil', nouveau:teteBroute};
-				this.teteActuelle = "teteBroute";
-			}else{
-				console.log("Je suis en plein repas ! ");
-				this.changement = undefined; //aucun changement de tête
-			}
-			
 			//manger l'herbe
-			this.nourritureEnCours.mangeMoi();
-			console.log("Je broute" , this.teteActuelle);
-			//as-tu fini de manger l'herbe en cours ?
-			this.eating = this.nourritureEnCours.mangeable;
-			
-			//fini de brouter ?
-			if(!this.eating){
-				console.log("j'ai fini de manger !");
-				if(this.teteActuelle == "teteBroute"){
-					console.log("je doit changer : teteProfil");
-					this.changement = {oldClass:'teteBroute', nouveau:teteProfil};
-					this.teteActuelle = "teteProfil";
-				}
-			}
+			this.nourritureVisee.mangeMoi();
+			//console.log("Je broute" , this.teteActuelle);
+
+		}else{
+			//fini de brouter l'herbe en cours
+			this.eating = false;
 		}
+		
 		// var animer = document.getElementById("boucheBroute");
 		
 		//si vache de face
@@ -143,6 +111,45 @@ class Vache extends SCCube{
 		this.taille += 1;
 		this.bouse += 1;
 		this.lait += 1;
+		
+	}
+	
+	appetissante(nourriture){
+		return this.nourritureVisee.taille > 0.5;
+	}
+	
+	$actionForever_mouvementTete(){
+		if(!this.eating && this.teteActuelle != "teteProfil"){
+			consol("++++++++++++++++++++++++ ");
+			console.log("Je dois changer de tête ! ");
+			this.changement = {oldClass:this.teteActuelle , nouveau:teteProfil};
+			this.teteActuelle ='teteProfil';
+		}
+		//vérifie si la tête actuelle est celle par défaut (teteProfil)
+		console.log("Tête actuelle" , this.teteActuelle);
+		if(this.teteActuelle == "teteProfil"){
+			this.changement = undefined;
+		}else{
+			// this.changement = {oldClass:this.teteActuelle, nouveau:teteProfil};
+			// this.teteActuelle = "teteProfil";
+		}
+			//Dois-je changer de tête ?
+			if(this.teteActuelle == "teteProfil"){
+				console.log("Je commence à manger")
+				this.changement = {oldClass:'teteProfil', nouveau:teteBroute};
+				this.teteActuelle = "teteBroute";
+			}else{
+				console.log("Je suis en plein repas ! ");
+				this.changement = undefined; //aucun changement de tête
+			}
+			if(!this.eating){
+				console.log("j'ai fini de manger !");
+				if(this.teteActuelle == "teteBroute"){
+					console.log("je doit changer : teteProfil");
+					this.changement = {oldClass:'teteBroute', nouveau:teteProfil};
+					this.teteActuelle = "teteProfil";
+				}
+			}
 	}
 
 
